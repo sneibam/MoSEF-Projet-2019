2f664063931a76498c6c6865af6950294c34ce9d
Mohamed Sneiba HAMOUD <mohamedsneiba.hamoud@ensiie.fr>
2019-11-11 11:51:21 +0100

    Message lors du lancement du programme + entree de l'utilisateur

---
+++ search_fichiers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Recupere la date avec le bon format
+now=$(date +'%d %B %Y')
+#name=$USERNAME
+
+echo 'Bienvenue' $USERNAME ', nous sommes le ' $now
+
+echo "Quel répertoire vous intéresse aujourd'hui ?"
+
+read directory
+
+if [ ! -d $directory ]
+then
+	echo -ne "Le repertoire '"$directory"' n'existe pas !"
+	exit 4
+else
+	echo -ne "Vous avez choisi le repertoire '"$directory"'.\n"
+fi
+
