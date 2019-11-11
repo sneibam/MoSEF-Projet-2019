908570619add4fcfcfcf2d25beeee56341659dac
Mohamed Sneiba HAMOUD <mohamedsneiba.hamoud@ensiie.fr>
2019-11-11 12:15:36 +0100

    Verification des arguments

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
+# Verification du nombre d'arguments
+
+if [ $# -ne 1 ]
+then
+	echo -ne "Nombre de parametres incorrect !\nVoici la syntaxe correct => " $0 " pattern\n"
+	exit 2
+fi
+
+
 # Recupere la date avec le bon format
 now=$(date +'%d %B %Y')
 #name=$USERNAME
@@ -11,7 +20,7 @@ echo "Quel répertoire vous intéresse aujourd'hui ?"
 
 read directory
 
-if [ ! -d $directory ]
+if [ ! -d "$directory" ]
 then
 	echo -ne "Le repertoire '"$directory"' n'existe pas !"
 	exit 4
